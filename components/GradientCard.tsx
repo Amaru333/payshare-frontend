cb1934b6b634debefba8169eec5bb9181dcd3bba
Amaru333 <amrutesharun0599@gmail.com>
2024-07-02 16:18:24 +0530

    Added homepage

---
+++ components/GradientCard.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from "react-native";
+import React, { ReactNode } from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import { BlurView } from "expo-blur";
+
+const GradientCard = ({ children }: { children: ReactNode }) => {
+  return (
+    <LinearGradient colors={["rgba(103,39,153,0.14)", "rgba(84,59,237,0.14)"]} end={{ x: 0.35, y: 0.4 }} start={{ x: 0.85, y: 0.15 }} style={{ borderRadius: 16, flex: 1 }} locations={[0.5, 0.8]}>
+      <BlurView style={{ borderRadius: 16 }} intensity={40}>
+        {children}
+      </BlurView>
+    </LinearGradient>
+  );
+};
+
+export default GradientCard;
+
+const styles = StyleSheet.create({});
--- screens/homepage/index.tsx
+++ screens/homepage/index.tsx
@@ -1,71 +1,45 @@
-import { ImageBackground, SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import { Dimensions, Image, ImageBackground, SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { ThemedText } from "@/components/ThemedText";
 import DashboardContainer from "@/components/DashboardContainer";
+import GradientCard from "@/components/GradientCard";
+
+const { width } = Dimensions.get("window");
+const containerPadding = 12;
+const imageWidth = width - 2 * containerPadding;
 
 const Homepage = () => {
   return (
     <DashboardContainer>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepage</ThemedText>
-        <ThemedText>Homepag</ThemedText>
-        <ThemedText>Homepa</ThemedText>
-        <ThemedText>Homep</ThemedText>
-        <ThemedText>Home</ThemedText>
-        <ThemedText>Hom</ThemedText>
-        <ThemedText>Ho</ThemedText>
-        <ThemedText>H</ThemedText>
-        <ThemedText>H</ThemedText>
-        <ThemedText>H</ThemedText>
-        <ThemedText>H</ThemedText>
-        <ThemedText>H</ThemedText>
-        <ThemedText>H</ThemedText>
-        <ThemedText>H</ThemedText>
-        <ThemedText>H</ThemedText>
-        <ThemedText>H</ThemedText>
-        <ThemedText>H</ThemedText>
-        <ThemedText>H</ThemedText>
-        <ThemedText>H</ThemedText>
-        <ThemedText>H</ThemedText>
-        <ThemedText>H</ThemedText>
-        <ThemedText>H</ThemedText>
-        <ThemedText>H</ThemedText>
-        <ThemedText>H</ThemedText>
-        <ThemedText>H</ThemedText>
-        <ThemedText>H</ThemedText>
-        <ThemedText>H</ThemedText>
-        <ThemedText>H</ThemedText>
-        <ThemedText>H</ThemedText>
+      <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ padding: containerPadding, gap: 24 }}>
+        <ImageBackground source={require("../../assets/images/backgrounds/you-owe.png")} resizeMode="contain" style={styles.cardContainer}>
+          <ThemedText style={styles.title}>You owe</ThemedText>
+          <ThemedText style={[styles.subTitle, { color: "#AB41FF" }]}>$25,000</ThemedText>
+        </ImageBackground>
+        <ImageBackground source={require("../../assets/images/backgrounds/you-lent.png")} resizeMode="contain" style={[styles.cardContainer]}>
+          <ThemedText style={styles.title}>You lent</ThemedText>
+          <ThemedText style={[styles.subTitle, { color: "#543BED" }]}>$25,000</ThemedText>
+        </ImageBackground>
+        <View style={{ display: "flex", flexDirection: "row", gap: 24 }}>
+          <GradientCard>
+            <View style={styles.gradientCardContainer}>
+              <ThemedText style={styles.gradientCardTitle}>Transactions</ThemedText>
+              <ThemedText style={styles.gradientCardSubTitle}>this month</ThemedText>
+              <View style={styles.numberContainer}>
+                <ThemedText style={styles.number}>2,500</ThemedText>
+              </View>
+            </View>
+          </GradientCard>
+          <GradientCard>
+            <View style={styles.gradientCardContainer}>
+              <ThemedText style={styles.gradientCardTitle}>Amount Spent</ThemedText>
+              <ThemedText style={styles.gradientCardSubTitle}>this month</ThemedText>
+              <View style={styles.numberContainer}>
+                <ThemedText style={styles.number}>$25,000</ThemedText>
+              </View>
+            </View>
+          </GradientCard>
+        </View>
       </ScrollView>
     </DashboardContainer>
   );
@@ -73,4 +47,41 @@ const Homepage = () => {
 
 export default Homepage;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  cardContainer: {
+    width: imageWidth,
+    height: 0.355 * imageWidth,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 12,
+    borderRadius: 16,
+  },
+  title: {
+    fontFamily: "Poppins600",
+    fontSize: 32,
+    lineHeight: 40,
+  },
+  subTitle: {
+    fontFamily: "Poppins600",
+    fontSize: 40,
+    lineHeight: 48,
+  },
+  gradientCardContainer: {
+    paddingVertical: 20,
+    paddingHorizontal: 12,
+  },
+  gradientCardTitle: { fontFamily: "Poppins600", fontSize: 20 },
+  gradientCardSubTitle: { fontFamily: "Poppins600", fontSize: 12, lineHeight: 14 },
+  numberContainer: {
+    display: "flex",
+    alignItems: "center",
+    paddingTop: 10,
+  },
+  number: {
+    fontFamily: "Poppins600",
+    fontSize: 32,
+    lineHeight: 40,
+  },
+});
